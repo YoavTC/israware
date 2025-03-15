3ce2ff496233b409f08d379bdc550315e630bb90
Yoav TC <yoavtc2004@gmail.com>
2025-03-15 14:09:05 +0200

    Improved juice & feel for Throw Keter Chair microgame

--- Assets/_Game Assets/Microgames/throwKeterChair/ArsAnimationHandler.cs
+++ Assets/_Game Assets/Microgames/throwKeterChair/ArsAnimationHandler.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+using MoreMountains.Feedbacks;
 using NaughtyAttributes;
 using UnityEditor.Animations;
 using UnityEngine;
@@ -11,10 +13,27 @@ namespace _Game_Assets.Microgames.throwKeterChair
         [SerializeField, AnimatorParam(nameof(animator))] private string hitParam, missParam;
         [SerializeField, AnimatorParam(nameof(animator))] private string randomParam;
 
+        [SerializeField] private MMF_Player feedbackPlayer, chairFeedbackPlayer;
+
         public void OnThrowChair(bool hit)
         {
             animator?.SetBool(randomParam, Random.RandomBool());
             animator?.SetTrigger(hit ? hitParam : missParam);
+        }
+
+        [UsedImplicitly] // Triggered by animation
+        public void PlayCameraShakeFeedback(float strength)
+        {
+            feedbackPlayer.GetFeedbackOfType<MMF_CameraShake>().CameraShakeProperties.AmplitudeX = strength;
+            feedbackPlayer.GetFeedbackOfType<MMF_CameraShake>().CameraShakeProperties.AmplitudeY = strength;
+            feedbackPlayer.GetFeedbackOfType<MMF_CameraShake>().CameraShakeProperties.AmplitudeZ = strength;
+            feedbackPlayer.PlayFeedbacks();
+        }
+
+        [UsedImplicitly] // Triggered by animation
+        public void PlayChairFeedback()
+        {
+            chairFeedbackPlayer.PlayFeedbacks();
         }
     }
 }
