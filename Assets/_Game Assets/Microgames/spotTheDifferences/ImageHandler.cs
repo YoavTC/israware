be47c737c9d32119ee3fac57f5c1594aecd215c4
Yoav TC <yoavtc2004@gmail.com>
2025-04-06 16:36:34 +0300

    Added base for Spot The Differences microgame

---
+++ Assets/_Game Assets/Microgames/spotTheDifferences/ImageHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityUtils;
+
+namespace _Game_Assets.Microgames.spotTheDifferences
+{
+    public class ImageHandler : MonoBehaviour
+    {
+        [SerializeField] private Transform pointsParent;
+        private const float X_OFFSET = 960f;
+
+        private void Awake()
+        {
+            Transform[] children = pointsParent.Children().ToArray();
+            for (int i = 0; i < children.Length; i++)
+            {
+                var childCopy = Instantiate(children[i], pointsParent);
+                Vector3 newPosition = childCopy.localPosition;
+                newPosition.x -= X_OFFSET;
+                childCopy.localPosition = newPosition;
+                
+                Transform childrenParent = new GameObject($"[{i}] points parent").transform;
+                
+                childrenParent.SetParent(pointsParent);
+                
+                childCopy.SetParent(childrenParent);
+                children[i].SetParent(childrenParent);
+            }
+        }
+    }
+}
---
+++ Assets/_Game Assets/Microgames/spotTheDifferences/InputHandler.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityUtils;
+
+namespace _Game_Assets.Microgames.spotTheDifferences
+{
+    public class InputHandler : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private Camera mainCamera;
+        [SerializeField] private Transform pointsParent;
+        [SerializeField] private Canvas canvas;
+        
+        [Header("Raycasting Components")]
+        [SerializeField] private GraphicRaycaster graphicRaycaster;
+        [SerializeField] private EventSystem eventSystem;
+        
+        [Header("Transition Settings")]
+        [SerializeField] private GameObject pointCirclePrefab;
+        [SerializeField] private Ease circleTransitionEase;
+        [SerializeField] private float circleTransitionDuration;
+        [SerializeField] private Vector2 circleTransitionStartEndScale;
+        
+        [Header("Events")]
+        [SerializeField] private UnityEvent circleLandUnityEvent;
+        [SerializeField] private UnityEvent spottedAllPointsUnityEvent;
+        
+        private PointerEventData pointerEventData;
+        private int targetLayer;
+        private List<RaycastResult> results = new List<RaycastResult>();
+
+        void Awake()
+        {
+            pointerEventData = new PointerEventData(eventSystem);
+            targetLayer = LayerMask.NameToLayer("Target");
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && GetClickedPoint(out Transform point))
+            {
+                SpotPoint(point);
+            }
+        }
+
+        private Transform GetClickedPoint(out Transform point)
+        {
+            results.Clear();
+            
+            pointerEventData.position = Input.mousePosition;
+            graphicRaycaster.Raycast(pointerEventData, results);
+            
+            point = null;
+            
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (results[i].gameObject.layer == targetLayer)
+                {
+                    point = results[i].gameObject.transform;
+                    break;
+                }
+            }
+            
+            return point;
+        }
+
+        private void SpotPoint(Transform point)
+        {
+            Transform pointsHolder = point.parent;
+                
+            foreach (Transform pointChild in pointsHolder.Children())
+            {
+                Transform circle = Instantiate(pointCirclePrefab, pointChild.position, Quaternion.identity, canvas.transform).transform;
+                    
+                circle.localScale = circleTransitionStartEndScale.x * pointChild.localScale;
+                circle.GetComponent<Image>().DOFade(1f, circleTransitionDuration).SetEase(circleTransitionEase);
+                circle.DOScale(circleTransitionStartEndScale.y * pointChild.localScale, circleTransitionDuration)
+                    .SetEase(circleTransitionEase)
+                    .OnComplete(() => circleLandUnityEvent?.Invoke());
+            }
+            
+            pointsHolder.gameObject.SetActive(false);
+
+            if (!pointsParent.Children().Any(child => child.gameObject.activeSelf))
+            {
+                spottedAllPointsUnityEvent?.Invoke();
+            }
+        }
+    }
+}
