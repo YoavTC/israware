6a606fb7ff2a0c394130b77de827a1ac49808641
Yoav TC <yoavtc2004@gmail.com>
2025-05-20 12:28:37 +0300

    Added dynamic shopping item spawning & new items

--- Assets/_Game Assets/Microgames/shoppingList/ShelfItemsManager.cs
+++ Assets/_Game Assets/Microgames/shoppingList/ShelfItemsManager.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using EditorAttributes;
 using External_Packages.Extensions;
 using UnityEngine;
@@ -8,29 +8,68 @@ namespace _Game_Assets.Microgames.shoppingList
 {
     public class ShelfItemsManager : MonoBehaviour
     {
+        [Header("Components")]
+        [SerializeField] private ShoppingList shoppingList;
+        
+        [Header("Settings")]
+        [SerializeField, DisableInPlayMode] private int shoppingListSize;
+        [SerializeField, DisableInPlayMode] private int maxDecoys;
+        
+        [Header("Shelf Item Prefabs")]
         [SerializeField] private ShelfItem shelfItemPrefab;
         [SerializeField] private List<ShelfItemScriptableObject> shelfItems;
-        [Space]
+         
+        [Header("Shelf Item Settings")]
+        [SerializeField] private float shelfItemXMultiplier;
+        [SerializeField] private float shelfItemZOffset;
         [SerializeField] private Vector2[] shelfItemHeights;
         
-        [SerializeField] private Notebook notebook;
-
-        [Button]
-        public void SpawnItem()
-        {
-            ShelfItem shelfItem = Instantiate(shelfItemPrefab, transform);
-            shelfItem.Init(shelfItems.Random());
-        }
+        [SerializeField, HideInEditMode] private int itemsSpawned;
+        [SerializeField, HideInEditMode] private int lastSpawnedShelf;
 
         public void Start()
         {
-            for (int i = 0; i < 4; i++)
+            ShelfItemScriptableObject[] items = shelfItems.Shuffle().Take(shoppingListSize).ToArray();
+            ShelfItemScriptableObject[] decoys = shelfItems.Except(items).ToArray();
+
+            items.Shuffle();
+            decoys.Shuffle();
+            
+            shoppingList.FillShoppingList(items);
+            StockShelves(items, decoys);
+        }
+
+        private void StockShelves(ShelfItemScriptableObject[] items, ShelfItemScriptableObject[] decoys)
+        {
+            // Duplicate the decoys to fill the shelves, but limit to the max
+            decoys = decoys.Concat(decoys) // Duplicate
+                .ToArray().Shuffle() // Shuffle again
+                .Take(maxDecoys).ToArray(); // Limit to max decoys
+
+            // Duplicate the items to give the player more chances to find them
+            items = items.Concat(items).ToArray();
+            
+            items.Concat(decoys)
+                .ToArray()
+                .Shuffle()
+                .ForEach(SpawnItem);
+        }
+
+        private void SpawnItem(ShelfItemScriptableObject item)
+        {
+            ShelfItem shelfItem = Instantiate(shelfItemPrefab, transform);
+            shelfItem.Init(item);
+            
+            itemsSpawned++;
+            
+            int randomShelf = UnityEngine.Random.Range(0, shelfItemHeights.Length);
+            if (randomShelf == lastSpawnedShelf) 
             {
-                ShelfItemScriptableObject item = shelfItems.Random();
-                shelfItems.Remove(item);
-                
-                notebook.AddItem(item);
+                randomShelf = (randomShelf + 1) % shelfItemHeights.Length;
             }
+            lastSpawnedShelf = randomShelf;
+
+            shelfItem.transform.position = new Vector3(itemsSpawned * shelfItemXMultiplier, shelfItemHeights[randomShelf].y, shelfItemZOffset);
         }
     }
 }
---
+++ Assets/_Game Assets/Microgames/shoppingList/ShoppingList.cs
@@ -0,0 +1,24 @@
+using External_Packages.Extensions;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Game_Assets.Microgames.shoppingList
+{
+    public class ShoppingList : MonoBehaviour
+    {
+        public void FillShoppingList(ShelfItemScriptableObject[] items)
+        {
+            foreach (ShelfItemScriptableObject item in items)
+            {
+                var itemSlot = new GameObject("ItemSlot");
+                itemSlot.transform.SetParent(transform, false);
+
+                itemSlot.GetOrAdd<RectTransform>();
+                itemSlot.GetOrAdd<CanvasRenderer>();
+                itemSlot.GetOrAdd<Image>().sprite = item.itemSprite;
+                
+                Debug.Log($"Added {item.name} to shopping list");
+            }
+        }
+    }
+}
