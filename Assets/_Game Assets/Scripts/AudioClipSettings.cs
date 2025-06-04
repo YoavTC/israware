747ca510a9b3521c3933ef510a961646f072e723
Yoav TC <yoavtc2004@gmail.com>
2025-06-04 12:50:58 +0300

    Added SFX Manager script

---
+++ Assets/_Game Assets/Scripts/AudioClipSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Game_Assets.Scripts
+{
+    [Serializable]
+    public class AudioClipSettings
+    {
+        public AudioClip audioClip;
+        public float volume;
+        public float pitch;
+
+        public AudioClipSettings(AudioClip audioClip, float volume = 1f, float pitch = 1f)
+        {
+            this.audioClip = audioClip;
+            this.volume = volume;
+            this.pitch = pitch;
+        }
+    }
+}
---
+++ Assets/_Game Assets/Scripts/SFXManager.cs
@@ -0,0 +1,44 @@
+using _Game_Assets.Scripts.Definitions;
+using UnityEngine;
+
+namespace _Game_Assets.Scripts
+{
+    public class SFXManager : MonoBehaviour, IMicrogameCallbacksListener
+    {
+        [Header("Music")] 
+        [SerializeField] private AudioClip backgroundMusic;
+        [SerializeField, Range(0f, 1f)] private float backgroundMusicVolume;
+
+        public void PlaySFX(AudioClip audioClip)
+        {
+            PlaySFX(new AudioClipSettings(audioClip));
+        }
+    
+        public void PlaySFX(AudioClipSettings audioClipSettings)
+        {
+            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = audioClipSettings.audioClip;
+            audioSource.volume = audioClipSettings.volume;
+            audioSource.pitch = audioClipSettings.pitch;
+        
+            audioSource.Play();
+            Destroy(audioSource, audioClipSettings.audioClip.length);
+        }
+    
+        public void OnMicrogameLoaded(MicrogameScriptableObject microgame) => StopALlSounds();
+        public void OnMicrogameFinished(bool result) => StopALlSounds();
+    
+        private void StopALlSounds()
+        {
+            AudioSource[] audioSources = GetComponents<AudioSource>();
+            foreach (AudioSource audioSource in audioSources)
+            {
+                if (audioSource.isPlaying)
+                {
+                    audioSource.Stop();
+                }
+                Destroy(audioSource);
+            }
+        }
+    }
+}
