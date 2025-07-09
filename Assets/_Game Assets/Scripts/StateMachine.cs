3c4fc4d326afdea551d4139decea8fc8a1a3e3da
Yoav TC <yoavtc2004@gmail.com>
2025-07-09 16:50:26 +0300

    Redid Main scene & Game Manager script

---
+++ Assets/_Game Assets/Scripts/StateMachine.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using _Game_Assets.Scripts.Definitions;
+using EditorAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Game_Assets.Scripts
+{
+    public enum State
+    {
+        GAME,
+        STATUS,
+        DEATH,
+    }
+    
+    public class StateMachine : MonoBehaviour
+    {
+        [Header("Managers")] 
+        [SerializeField] private MicrogameProvider microgameProvider;
+        [SerializeField] private StatusScreen statusScreen;
+        
+        private AsyncOperation gameSceneLoadOperation;
+        private bool lastGameResult;
+        
+        [SerializeField] private State currentState;
+
+        private void Start()
+        {
+            gameSceneLoadOperation = null;
+            
+            PrepareGame();
+            ChangeState(State.GAME);
+        }
+
+        [Button]
+        public void OnMicrogameFinished(bool won)
+        {
+            lastGameResult = won;
+            ChangeState(State.STATUS);
+        }
+
+        [Button]
+        public void ChangeState(State state)
+        {
+            Debug.Log($"Received new state [{state}]");
+            
+            switch (state)
+            {
+                case State.GAME:
+                    StartCoroutine(LoadGame());
+                    break;
+                case State.STATUS:
+                    PrepareGame();
+                    ShowStatus();
+                    break;
+                case State.DEATH:
+                    Death();
+                    break;
+            }
+            
+            Debug.Log($"Changing current state to [{state}]");
+            currentState = state;
+        }
+
+        private void PrepareGame()
+        {
+            MicrogameScriptableObject microgame = microgameProvider.GetMicrogame();
+            
+            // Load the scene asynchronously
+            var loadSceneAsync = SceneManager.LoadSceneAsync(microgame.id);
+            if (loadSceneAsync != null)
+            {
+                // Disable automatic scene switching
+                loadSceneAsync.allowSceneActivation = false;
+                gameSceneLoadOperation = loadSceneAsync;
+            }
+        }
+
+        private IEnumerator LoadGame()
+        {
+            Debug.Log("Loading game scene...");
+            yield return new WaitUntil(() => gameSceneLoadOperation.progress >= 0.9f);
+            
+            // Allow the scene switch
+            gameSceneLoadOperation.allowSceneActivation = true;
+
+            Debug.Log("Game scene loaded");
+            yield return null; // Wait for the next frame to ensure that the scene load operation is complete
+
+            statusScreen.HideStatus();
+            gameSceneLoadOperation = null;
+        }
+        
+        private void ShowStatus()
+        {
+            Debug.Log("Showing status screen");
+            statusScreen.ShowStatus(lastGameResult);
+        }
+
+        private void Death()
+        {
+            Debug.Log("Death state reached, showing death screen");
+        }
+    }
+}
---
+++ Assets/_Game Assets/Scripts/StatusScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace _Game_Assets.Scripts
+{
+    public class StatusScreen : MonoBehaviour
+    {
+        [SerializeField] private StateMachine stateMachine;
+        
+        [SerializeField] private GameObject screenParent;
+        [SerializeField] private VideoPlayer videoPlayer;
+        [SerializeField] private RenderTexture renderTexture;
+        
+        [SerializeField] private VideoClip positiveVideoClip;
+        [SerializeField] private VideoClip negativeVideoClip;
+
+        public void ShowStatus(bool won)
+        {
+            videoPlayer.clip = won ? positiveVideoClip : negativeVideoClip;
+            
+            screenParent.SetActive(true);
+            videoPlayer.Play();
+            videoPlayer.loopPointReached += source => stateMachine.ChangeState(State.GAME);
+        }
+
+        public void HideStatus()
+        {
+            screenParent.SetActive(false);
+            videoPlayer.clip = null;
+            renderTexture.Release();
+        }
+    }
+}
